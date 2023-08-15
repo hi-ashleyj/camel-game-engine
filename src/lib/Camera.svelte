61060158ea30887ee11a3b726eb7559163c10140
Ashley J <ajor023@gmail.com>
2023-08-15 01:39:15 +0000

    help

--- src/lib/Camera.svelte
+++ src/lib/Camera.svelte
@@ -1,15 +1,22 @@
 <script lang="ts">
 
     import { T } from "@threlte/core";
+    import * as THREE from "three";
+    import { Euler } from "three";
+
+    let something = new THREE.PerspectiveCamera();
+    something.rotation
 
     export let fov = 70;
     export let target = false;
     export let position: [ number, number, number ] = [ 5, 5, 5 ];
+    export let scale: [ number, number, number ] = [ 1, 1, 1 ];
+    export let rotation: [ number, number, number ] = [ 0, 0, 0 ];
 
 </script>
 
-<T.PerspectiveCamera {position} fov={fov} makeDefault={target} on:create={({ref}) => { ref.lookAt(0, 0, 0 )} } >
+<T.PerspectiveCamera {position} {scale} rotation={[ ...rotation, Euler.DEFAULT_ORDER ]} fov={fov} makeDefault={target} let:ref={ref} >
     
-    <slot />
+    <slot {ref} />
 
 </T.PerspectiveCamera>
